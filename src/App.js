d791826f6f799dbec92da7a3e9589162426b246e
PvrkFvmily <wlsgnl9973@gmail.com>
2023-01-25 08:46:52 -0800

    Daniel's diy-api-client

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/pages/Home';
+import NewBlog from './components/pages/NewBlog';
+import Navbar from './components/Navbar';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route
+            path='/'
+            element={<Home />}
+          />
+          <Route
+            path='/new-blog'
+            element={<NewBlog />}
+          />
+        </Routes>
+      </main>
+    </Router>
   );
 }
 
---
+++ src/components/pages/NewBlog.jsx
@@ -0,0 +1,5 @@
+export default function NewBlog () {
+    return (
+        <h1>Make new blog</h1>
+    )
+}
